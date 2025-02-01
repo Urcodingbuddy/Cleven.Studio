ae1d6b839f21f19840ae389ac1a23c90ae945331
Arav Menon <aravmenon.ak@gmail.com>
2025-02-01 19:51:08 +0530

    updated Nav, logo, favicon, added font, some changes in package.json port

--- apps/Client/app/page.tsx
+++ apps/Client/app/page.tsx
@@ -1,11 +1,13 @@
+import { AppBar } from "@repo/components/AppBar";
 import { CirculerBtn }  from "@repo/components/CirculerBtn";
+
 
 
 
 export default function Home() {
   return (
     <>
-    <CirculerBtn>Join us</CirculerBtn>
+    <AppBar/>
     </>
   );
 }

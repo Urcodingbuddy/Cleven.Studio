40efeb346deb563e5fca77658ae68ba6cbc784c7
Arav Menon <aravmenon.ak@gmail.com>
2025-02-07 04:07:20 +0530

    styling to EssectionalSection

--- apps/Client/components/src/EssentialSection.tsx
+++ apps/Client/components/src/EssentialSection.tsx
@@ -2,8 +2,8 @@ export const EssentialSection = () => {
  return(
     <>
     <section>
-      <div id="Essential" className="text-white h-screen w-screen " >
-         <div id="essential_text">
+      <div id="Essential" className="text-white min-h-screen w-screen p-24" >
+         <div id="essential_text" className="" >
           <h1 className="text-5xl font-bold ">Everything you need.</h1>
           <h2 className="text-5xl font-bold">Nothing you don't</h2>
             <p className="text-xl mt-7 " >
@@ -11,8 +11,12 @@ export const EssentialSection = () => {
             Experience tailored solutions that simplify every aspect of your <br/>
             online presence.
             </p>
-
          </div>
+
+         <div id="Elements" className="text-center" >
+            <h1 className="text-6xl font-bold mt-30 " >ADD ACETERNITY UI </h1>
+         </div>
+
 
       </div>
     </section>

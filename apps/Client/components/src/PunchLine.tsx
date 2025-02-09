489ed412e0370367cc42bc293ed2eb19229ae18a
Urcodingbuddy <anshpethe110@gmail.com>
2025-02-09 22:03:28 +0530

    background beam's and JoinUs

--- apps/Client/components/src/PunchLine.tsx
+++ apps/Client/components/src/PunchLine.tsx
@@ -1,17 +1,20 @@
-
+import { BackgroundBeamsWithCollision } from "../../@/components/ui/background-beams-with-collision";
+import { ArrowDown } from "lucide-react";
 export const PunchLine = () => {
     return (
-        <div className="mt-60 text-center w-[90vw] mx-auto">
+    <BackgroundBeamsWithCollision>
+        <div className="mt-60 text-center w-[90vw] mx-auto relative">
 
             <h1 className=" text-4xl sm:text-6xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 mb-10">
                 Unleashing the Power of Performance <br /> to Make Your Website Invincible.</h1>
                 <div>
                 <p className={`text-zinc-400 text-xl leading-8 font-medium mb-20`}>Say goodbye to messy website management. Cleven Studios makes maintaining, <br/> optimizing, and enhancing your site effortless. Reliable. Scalable. Stress-free. <br /> Simple. Intuitive. And never boring.</p>
-                <div className="mb-16 flex flex-col items-center">
-                <p className="text-md font-semibold text-zinc-400 hover:underline hover:font-bold  "><a href="#">Learn more</a></p>
-                <img className="h-6 w-6 mt-2" src="./downArrow.svg" alt="" />
+                <div className="mb-16 flex flex-col items-center group cursor-pointer">
+                <p className="text-md font-semibold text-zinc-400 hover:font-bold "><a href="#">Learn more</a></p>
+                <ArrowDown className="group-hover:translate-y-2 text-white duration-500 transition-transform "/>
                 </div>
                 </div>
         </div>
+    </BackgroundBeamsWithCollision>
     )
 }

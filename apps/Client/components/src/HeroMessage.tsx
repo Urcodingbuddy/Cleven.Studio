7b51514c4f6f47366123b3c487d7259b3a730871
Arav Menon <aravmenon.ak@gmail.com>
2025-02-13 17:27:44 +0530

    Adjust sizes

--- apps/Client/components/src/HeroMessage.tsx
+++ apps/Client/components/src/HeroMessage.tsx
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/main
 export const HeroMessage = () => {
     return (
         <>
             <section id="heroMessage_head">
-<<<<<<< HEAD
-                <div id="intoMssg" className="h-[40rem] w-screen flex justify-center items-center  mx-auto">
-                    <div className="max-w-7xl w-full flex gap-5">
-                        <div id="leftMssg" className="w-1/2 h-full text-white flex">
-                        <h1 className="text-[3rem] font-black z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 leading-14">Who said maintaining a website has to be hassle?</h1>
-=======
                 <div id="intoMssg" className="h-[40rem] w-full px-4 sm:px-6 lg:px-8 max-w-7xl flex justify-center items-center mx-auto">
                     <div className="max-w-7xl w-full flex flex-col md:flex-row gap-5 px-4">
                         <div id="leftMssg" className="w-full lg:w-1/2 h-full text-white">
-                            <h1 className="text-4xl sm:text-5xl  font-bold flex z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+                            <h1 className="text-4xl sm:text-5xl font-bold flex z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
                                 Who said maintaining a website has to be a hassle?
                             </h1>
->>>>>>> refs/remotes/origin/main
                         </div>
                         <div id="rightMssg" className="w-full lg:w-1/2 h-full text-white lg:justify-end flex">
                             <div className="sm:text-lg md:text-xl font-medium leading-7 w-full lg:w-18/20"> 

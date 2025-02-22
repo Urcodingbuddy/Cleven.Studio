20f04625205493ef63f5fc2505cfa2e865d3bffe
Urcodingbuddy <anshpethe110@gmail.com>
2025-02-22 15:27:07 +0530

    pricing page sample

---
+++ apps/Client/app/(home)/pricing/page.tsx
@@ -0,0 +1,21 @@
+
+export default function PricingPage() {
+ 
+
+    return (
+      <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4">Pricing Information</h1>
+        
+        <div className="border border-gray-200 rounded-lg p-4">  
+        All Prices
+        </div>
+  
+        <button 
+          className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          All Subscription
+        </button>
+      </div>
+    );
+  }
+  

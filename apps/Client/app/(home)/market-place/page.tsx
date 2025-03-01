f08abd49817b12ce07e0fc9e8123ed163df8ab53
Urcodingbuddy <anshpethe110@gmail.com>
2025-03-01 20:48:08 +0530

    Added blank market-place

---
+++ apps/Client/app/(home)/market-place/page.tsx
@@ -0,0 +1,9 @@
+export default function MarketPlace() {
+    return (
+        <main className="min-h-screen relative flex items-center justify-center px-4 overflow-hidden">
+            <div className="w-full max-w-md space-y-8 relative">
+                <h1>Market Place</h1>
+            </div>
+        </main>
+    )
+}

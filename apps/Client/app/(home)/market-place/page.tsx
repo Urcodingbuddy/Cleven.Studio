ab0d39af2f55c5b9a259739172887eda791a4c9b
Arav Menon <aravmenon.ak@gmail.com>
2025-03-02 16:33:52 +0530

    changes in market place

--- apps/Client/app/(home)/market-place/page.tsx
+++ apps/Client/app/(home)/market-place/page.tsx
@@ -1,16 +1,14 @@
 export default function Home() {
   return (
-    <>
-      <main className="ml-18">
-        <div>
-          <div id="logo" className="p-6" >
-            <h1 className="text-white text-2xl " >Market Place </h1>
-          </div>
-
-
-
+    <main className="ml-18">
+      <div>
+        <div id="logo" className="p-6">
+          <h1 className="text-white text-2xl ">Market Place </h1>
         </div>
-      </main>
-    </>
+
+        
+
+      </div>
+    </main>
   );
 }

14c08e9bcc009fce3f17f7cec8b6baa0c604d438
Urcodingbuddy <anshpethe110@gmail.com>
2025-03-10 20:56:45 +0530

    Implimented Accenturnity Sidebar

---
+++ apps/Client/@/components/ui/sideBarV3.tsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "./sidebarClient";
+import { Radar, Store,  UploadCloud as CloudUpload, ReceiptText } from "lucide-react";
+import Link from "next/link";
+import { motion } from "motion/react";
+import { cn } from "../../lib/utils";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function SidebarV3({children}: {children: React.ReactNode}) {
+  const { data: session } = useSession();
+  const links = [
+    {
+      label: "WorkSpace",
+      href: "/workspace",
+      icon: (
+        <Radar className="text-neutral-700 dark:text-neutral-200 h-5.5 w-5.5 shrink-0" />
+      ),
+    },
+    {
+      label: "Upload",
+      href: "/upload",
+      icon: (
+        <CloudUpload className="text-neutral-700 dark:text-neutral-200 h-5.5 w-5.5 shrink-0" />
+      ),
+    },
+    {
+      label: "Billing",
+      href: "/billing",
+      icon: (
+        <ReceiptText className="text-neutral-700 dark:text-neutral-200 h-5.5 w-5.5 shrink-0" />
+      ),
+    },
+    {
+      label: "Market Place",
+      href: "/marketplace",
+      icon: (
+        <Store className="text-neutral-700 dark:text-neutral-200 h-5.5 w-5.5 shrink-0" />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      className={cn(
+        "flex flex-col md:flex-row bg-neutral-900 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700",
+        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-16 flex flex-col gap-10">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <SidebarLink
+              link={{
+                label: session?.user?.name || 'Loading...',
+                href: "/profile",
+                icon: session?.user?.image ? (
+                  <Image
+                    src={session.user.image} 
+                    className="rounded-full border border-white" 
+                    alt="Profile" 
+                    width={30}
+                    height={30}
+                  />
+                ) : (
+                  <Image
+                    src={`https://robohash.org/${session?.user?.name}.png?size=30x30`} 
+                    className="rounded-full border"  
+                    alt="" 
+                    width={30}
+                    height={30}
+                  />
+                )
+              }}
+            />
+          </div>
+        </SidebarBody>
+      </Sidebar> 
+      {children}
+    </div>
+  );
+}
+export const Logo = () => {
+  return (
+    <Link
+      href="/"
+      className="font-normal flex space-x-4 items-center text-sm text-black  relative z-20"
+    >
+      <img src="./Cleven removeBg.png" className="h-7 object-cover" alt="" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        CLEVEN.STUDIO
+      </motion.span>
+    </Link>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black relative z-20"
+    >
+      <img src="./Cleven removeBg.png" className="h-7 object-cover" alt="" />
+    </Link>
+  );
+};
+

8466d62eb5e382e7431ce7d3779af9c1a14e3545
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-09 10:10:17 +0530

    Update Home component: add InitialLoader for improved user experience during loading

--- apps/Client/app/page.tsx
+++ apps/Client/app/page.tsx
@@ -8,11 +8,13 @@ import { Footer } from "@repo/landing/Footer";
 import { Services } from "@repo/landing/Services";
 import { Testimonials } from "@repo/landing/Testimonials";
 import { TextHoverEffect } from "../@/components/ui/text-hover-effect";
+import { InitialLoader } from "@repo/landing/InitialLoader";
 
 export default function Home() {
 
   return (
     <>
+    <InitialLoader />
     <AppBar/>
     <SmoothScrollProvider/>
     <ContainerScroll>

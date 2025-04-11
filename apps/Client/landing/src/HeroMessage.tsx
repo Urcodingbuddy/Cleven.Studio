57140f40c8f0afc82985abf9ed2bbeea0350647c
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-11 09:56:29 +0530

    Update typography across multiple components: adjust heading sizes for improved readability

--- apps/Client/landing/src/HeroMessage.tsx
+++ apps/Client/landing/src/HeroMessage.tsx
@@ -7,7 +7,7 @@ export const HeroMessage = () => {
                 <div id="intoMsg" className="h-[40rem] w-full px-4 sm:px-6 lg:px-8 max-w-7xl flex justify-center items-center mx-auto">
                     <div className="max-w-7xl w-full flex flex-col md:flex-row gap-5 px-4">
                         <div id="leftMssg" className="w-full lg:w-1/2 h-full text-white">
-                            <h1 className="text-4xl sm:text-5xl  font-bold flex z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+                            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold flex z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
                                 Who said maintaining a website has to be a hassle?
                             </h1>
                         </div>

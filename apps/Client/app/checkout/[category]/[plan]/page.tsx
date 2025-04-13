4a8703a03660023692b620cd46b6e341e5b0aa53
Arav Menon <aravmenon.ak@gmail.com>
2025-04-13 17:53:21 +0530

    remove gradient code

--- apps/Client/app/checkout/[category]/[plan]/page.tsx
+++ apps/Client/app/checkout/[category]/[plan]/page.tsx
@@ -42,15 +42,7 @@ export default async function CheckoutPage({ params }: {
   const { features } = selectedPlan;
   return (
     <div className="min-h-screen bg-black text-white p-4 sm:p-6 md:p-8">
-      {/* <div className="fixed inset-0 bg-[#0c0c0c]">
-        <div className="absolute inset-0 opacity-30">
-          <div className="absolute top-[-10%] left-[-20%] w-[500px] h-[500px] rounded-b-lg bg-white blur-[100px] "></div>
-          <div className="absolute bottom-[-10%] right-[-10%] w-[500px] h-[500px]  bg-white blur-[100px]"></div>
-          <div className="absolute top-[40%] right-[20%] w-[400px] h-[400px] rounded-full bg-[#0c0c0c] blur-[100px] animate-pulse delay-700"></div>
-        </div>
-      </div> */}
       <SmoothScrollProvider />
-
 
       <div className="max-w-6xl mx-auto">
 

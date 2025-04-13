5c2a86758fe15d3faec333297d39cc4598ff200e
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-13 18:09:14 +0530

    Refactor GettingStarted component to use Link for navigation and clean up unused router logic

--- apps/Client/@/components/ui/GettingStarted.tsx
+++ apps/Client/@/components/ui/GettingStarted.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function GettingStarted({
@@ -8,18 +9,15 @@ export default function GettingStarted({
   category: string;
   plan: string;
 }) {
-const router = useRouter();
-
-const handleCheckout = () => {
-    router.push(`/checkout/${category}/${plan}`);
-};
+  const router = useRouter();
 
   return (
+    <Link href={`/checkout/${category}/${plan}`}>
     <button
-      onClick={handleCheckout}
       className={`w-full py-2.5 md:py-3 px-4 rounded-full text-sm md:text-base cursor-pointer font-medium transition-all duration-200 bg-white/10 hover:bg-white/20 text-white shadow-md shadow-black/20 hover:shadow-lg hover:shadow-black/30 active:scale-95 active:shadow-sm active:shadow-black/10`}
     >
       Get Started
     </button>
+    </Link>
   );
 }
